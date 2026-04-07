6c90215ce538290f5a3f791a8faa4709e9fcb99d
MIMIIK <injan9949@gmail.com>
2026-04-07 10:01:59 +0545

    smartbasket resolved

--- frontend/src/pages/SmartBasket.jsx
+++ frontend/src/pages/SmartBasket.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import apiClient from '../utils/api';
-import { useCart } from '../context/CartContext';
+import { useCart } from "../contexts/CartContext";
 import { bulkAddToCart } from '../utils/cartApi';
 import '../styles/SmartBasket.css';
 
